f41ceb978fd4d4cae63b37614f9ffa3056713529
haikel <haikelbrinis@gmail.com>
2021-09-09 22:27:11 +0100

    - integration templeate - add Ts config - add datatable config

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        dd(5);
+        return  $this->render('default/index.html.twig');
     }
 }
